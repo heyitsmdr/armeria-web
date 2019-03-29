c01413b32b94db952d5fe5dc3853dc821047cd32
Mike DuRussel - MDR <mdurussel@squarespace.com>
2019-03-29 08:48:51 -0400

    Start working on the base App component to create a base layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-top">
+      <div class="container-left">Top Left</div>
+      <div class="container-center">Top Center</div>
+      <div class="container-right">Top Right</div>
+    </div>
+    <div class="container-bottom">Bottom</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+html, body {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  background-color: #111;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Comfortaa', cursive;
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  font-size: 14px;
+  margin: 0;
+  padding: 0;
+  color: #dbe3e6;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  .container-top {
+    flex-grow: 1;
+    display: flex;
+
+    .container-left {
+      flex-basis: 250px;
+    }
+
+    .container-center {
+      flex-grow: 1;
+    }
+
+    .container-right {
+      flex-basis: 250px;
+    }
+  }
+
+  .container-bottom {
+    flex-basis: 300px;
+  }
 }
 </style>
