9558eef4e342780a22c80e6d17bcd592b6496607
Mike DuRussel - MDR <mdurussel@squarespace.com>
2019-05-13 10:00:03 -0400

    Revert

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,4 +2,11 @@
 
 sleep 2
 
-systemctl restart armeria
+cd /opt/armeria
+
+./build/armeria \
+    -data=/opt/armeria/data \
+    -public=/opt/armeria/client/dist \
+    -scripts=/opt/armeria/scripts \
+    -port=8081 \
+    >> /var/log/armeria.log 2>&1 &
