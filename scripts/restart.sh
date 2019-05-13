4c3fd9437c681eb76a75367c017c80fb33fb5045
Mike DuRussel - MDR <mdurussel@squarespace.com>
2019-05-13 09:49:58 -0400

    Switch restart to systemd

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,11 +2,4 @@
 
 sleep 2
 
-cd /opt/armeria
-
-./build/armeria \
-    -data=/opt/armeria/data \
-    -public=/opt/armeria/client/dist \
-    -scripts=/opt/armeria/scripts \
-    -port=8081 \
-    >> /var/log/armeria.log 2>&1 &
+systemctl restart armeria
