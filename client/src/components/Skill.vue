2c7a15a04ecfc3edc5c37f4f995cbfa65e1ed623
Mike DuRussel <heyitsmdr@gmail.com>
2019-09-07 15:37:29 -0400

    Add the initial ui for character skills

---
+++ client/src/components/Skill.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="skill-container">
+        <div class="name-container">
+            <div class="name">{{ name }}</div>
+            <div class="level">{{ level }}</div>
+        </div>
+        <div class="bar-container">
+            <div class="bar" ref="bar"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Skill',
+        props: ['name', 'level', 'percent'],
+        mounted: function() {
+            this.$refs['bar'].style.width = `${this.percent}%`;
+        },
+    }
+</script>
+
+<style scoped>
+    .skill-container {
+        display: flex;
+        flex-direction: column;
+        padding: 5px 10px;
+    }
+
+    .skill-container:hover {
+        background-color: #111;
+        cursor: pointer;
+    }
+
+    .skill-container .name-container {
+        display: flex;
+    }
+
+    .skill-container .name-container .name {
+        flex-grow: 1;
+        font-size: 13px;
+        font-weight: 600;
+    }
+
+    .skill-container .name-container .level {
+        flex-basis: 50px;
+        text-align: right;
+        padding-right: 2px;
+    }
+
+    .skill-container .bar-container {
+        border: 1px solid #3a3a3a;
+        box-shadow: inset 0px 0px 4px #000;
+        height: 10px;
+        margin-top: 3px;
+    }
+
+    .skill-container .bar-container .bar {
+        height: 100%;
+        background-color: #424242;
+        box-shadow: inset 0px 0px 2px #000;
+    }
+</style>
--- client/src/components/Skills.vue
+++ client/src/components/Skills.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-        Skills
+    <div class="skills-container">
+        <div class="skill-level-container">
+            <div class="text">Skill Level</div>
+            <div class="level">100</div>
+        </div>
+        <div class="skill-items-container">
+            <Skill name="Woodworking" level="15" percent="20" />
+            <Skill name="Climbing" level="20" percent="50" />
+        </div>
     </div>
 </template>
 
 <script>
+    import Skill from '@/components/Skill';
+
     export default {
         name: 'Skills',
+        components: { Skill },
     }
 </script>
 
 <style scoped>
+    .skills-container {
+        display: flex;
+        flex-direction: column;
+    }
 
+    .skills-container .skill-level-container {
+        flex-basis: 40px;
+        display: flex;
+        box-shadow: 0px 0px 7px #000;
+    }
+
+    .skills-container .skill-level-container .text {
+        flex-grow: 1;
+        display: flex;
+        align-items: center;
+        font-size: 18px;
+        padding-left: 10px;
+        background-color: #222;
+    }
+
+    .skills-container .skill-level-container .level {
+        flex-basis: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+        font-weight: 600;
+        background-color: #111;
+        color: #03A9F4;
+    }
+
+    .skills-container .skill-items-container {
+        flex-grow: 1;
+    }
 </style>
