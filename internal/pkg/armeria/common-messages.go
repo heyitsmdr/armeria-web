1efd22f55ed8b6d877f51e70a9d38e33f1274d6a
Mike DuRussel <heyitsmdr@gmail.com>
2019-09-08 13:42:36 -0400

    Add a common messages file for making sure common phrases are consistent

---
+++ internal/pkg/armeria/common-messages.go
@@ -0,0 +1,6 @@
+package armeria
+
+const (
+	CommonTargetNotFoundHere      string = "You don't see anyone here by that name."
+	CommonItemNotFoundOnCharacter string = "You don't have an item by that name."
+)
