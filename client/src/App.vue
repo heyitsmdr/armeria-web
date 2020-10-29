d1557bfe5eb89906c308b43b55e2b4c179a6fd04
Mike DuRussel <mdurussel@squarespace.com>
2020-10-28 21:59:33 -0400

    Allow pressing Shift + <direction> to create rooms

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,197 +1,201 @@
 <template>
-  <div id="app">
-    <div class="container-wrapper">
-      <div class="container-left">
-        <div class="container-minimap">
-          <Minimap />
+    <div id="app">
+        <div class="container-wrapper">
+            <div class="container-left">
+                <div class="container-minimap">
+                    <Minimap />
+                </div>
+                <div class="container-targets">
+                    <RoomTargets />
+                </div>
+            </div>
+            <div class="container-center">
+                <div class="container-maintext">
+                    <MainText :windowHeight="windowHeight" />
+                </div>
+                <div class="container-input">
+                    <InputBox />
+                </div>
+                <div class="container-bars">
+                    <Vitals />
+                </div>
+            </div>
+            <div class="container-right">
+                <div class="container-skills">
+                    <Skills />
+                </div>
+                <div class="container-inventory">
+                    <Inventory />
+                </div>
+            </div>
         </div>
-        <div class="container-targets">
-          <RoomTargets />
+        <div class="status-bar-container">
+            <StatusBar />
         </div>
-      </div>
-      <div class="container-center">
-        <div class="container-maintext">
-          <MainText :windowHeight="windowHeight" />
-        </div>
-        <div class="container-input">
-          <InputBox />
-        </div>
-        <div class="container-bars">
-          <Vitals />
-        </div>
-      </div>
-      <div class="container-right">
-        <div class="container-skills">
-          <Skills />
-        </div>
-        <div class="container-inventory">
-          <Inventory />
-        </div>
-      </div>
+        <ItemTooltip />
     </div>
-    <div class="status-bar-container">
-      <StatusBar />
-    </div>
-    <ItemTooltip />
-  </div>
 </template>
 
 <script>
-  import { mapState } from 'vuex';
-  import MainText from '@/components/MainText';
-  import InputBox from '@/components/InputBox';
-  import Minimap from '@/components/Minimap';
-  import RoomTargets from '@/components/RoomTargets';
-  import Inventory from '@/components/Inventory';
-  import Vitals from '@/components/Vitals';
-  import Skills from '@/components/Skills';
-  import StatusBar from '@/components/StatusBar';
-  import ItemTooltip from '@/components/ItemTooltip';
+import { mapState } from 'vuex';
+import MainText from '@/components/MainText';
+import InputBox from '@/components/InputBox';
+import Minimap from '@/components/Minimap';
+import RoomTargets from '@/components/RoomTargets';
+import Inventory from '@/components/Inventory';
+import Vitals from '@/components/Vitals';
+import Skills from '@/components/Skills';
+import StatusBar from '@/components/StatusBar';
+import ItemTooltip from '@/components/ItemTooltip';
 
-  export default {
+export default {
     name: 'App',
     components: {
-      InputBox,
-      MainText,
-      Minimap,
-      RoomTargets,
-      Inventory,
-      Vitals,
-      Skills,
-      StatusBar,
-      ItemTooltip
+        InputBox,
+        MainText,
+        Minimap,
+        RoomTargets,
+        Inventory,
+        Vitals,
+        Skills,
+        StatusBar,
+        ItemTooltip
     },
     data: () => {
-      return {
-        windowHeight: 0,
-        windowWidth: 0,
-      }
+        return {
+            windowHeight: 0,
+            windowWidth: 0,
+        }
     },
     computed: mapState(['allowGlobalHotkeys', 'objectEditorOpen', 'isConnected', 'playerInfo']),
     watch: {
-      isConnected: function(connected) {
-        let token = this.$store.state.autoLoginToken;
-        if (connected) {
-          this.$store.dispatch('showText', { data: `Welcome to Armeria!\n\n` });
+        isConnected: function(connected) {
+            let token = this.$store.state.autoLoginToken;
+            if (connected) {
+                this.$store.dispatch('showText', { data: `Welcome to Armeria!\n\n` });
 
-          if (token.length > 0) {
-            const char = token.split(':')[0];
-            this.$store.dispatch('showText', { data: `You are automatically being logged in as '${char}'.\n` });
-            this.$store.dispatch('sendSlashCommand', {
-              command: `/logintoken ${token}`
-            });
-          } else {
-            this.$store.dispatch('showText', { data: 'If you have an existing character, you can <b>/login</b>. Otherwise, <b>/create</b> a new one.\n' });
-          }
-        } else {
-          window.document.title = '**DISCONNECTED** Armeria.io';
+                if (token.length > 0) {
+                    const char = token.split(':')[0];
+                    this.$store.dispatch('showText', { data: `You are automatically being logged in as '${char}'.\n` });
+                    this.$store.dispatch('sendSlashCommand', {
+                        command: `/logintoken ${token}`
+                    });
+                } else {
+                    this.$store.dispatch('showText', { data: 'If you have an existing character, you can <b>/login</b>. Otherwise, <b>/create</b> a new one.\n' });
+                }
+            } else {
+                window.document.title = '**DISCONNECTED** Armeria.io';
+            }
+        },
+
+        playerInfo: function(info) {
+            window.document.title = `${info.name} - Armeria.io`;
         }
-      },
-
-      playerInfo: function(info) {
-        window.document.title = `${info.name} - Armeria.io`;
-      }
     },
     methods: {
-      onWindowResize() {
-        this.windowHeight = window.innerHeight;
-        this.windowWidth = window.innerWidth;
+        onWindowResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
 
-        document.querySelector('.container-center').style.maxWidth = `${this.windowWidth-500}px`;
-      },
+            document.querySelector('.container-center').style.maxWidth = `${this.windowWidth-500}px`;
+        },
 
-      onKeyUp(event) {
-        if (!this.allowGlobalHotkeys) {
-          return;
+        onKeyUp(event) {
+            if (!this.allowGlobalHotkeys) {
+                return;
+            }
+
+            let moveCommand = '';
+
+            switch(event.key.toLowerCase()) {
+                case 'w':
+                    moveCommand = "/move north";
+                    break;
+                case 'a':
+                    moveCommand = "/move west";
+                    break;
+                case 's':
+                    moveCommand = "/move south";
+                    break;
+                case 'd':
+                    moveCommand = "/move east";
+                    break;
+                case 'q':
+                    moveCommand = "/move down";
+                    break;
+                case 'e':
+                    moveCommand = "/move up";
+                    break;
+                case 'escape':
+                    this.$store.dispatch('setObjectTarget', '');
+                    break;
+                case 'enter':
+                    this.$store.dispatch('setForceInputFocus', { forced: true });
+                    break;
+                case '/':
+                    this.$store.dispatch('setForceInputFocus', { forced: true, text: '/' });
+                    break;
+            }
+
+            if (moveCommand.length > 0) {
+                if (event.shiftKey && this.$store.state.permissions.indexOf('CAN_BUILD')) {
+                    moveCommand = moveCommand.replace('/move', '/room create');
+                }
+
+                this.$store.dispatch('sendSlashCommand', {
+                    command: moveCommand
+                });
+            }
         }
-
-        let sendCommand = '';
-
-        switch(event.key.toLowerCase()) {
-          case 'w':
-            sendCommand = "/move north";
-            break;
-          case 'a':
-            sendCommand = "/move west";
-            break;
-          case 's':
-            sendCommand = "/move south";
-            break;
-          case 'd':
-            sendCommand = "/move east";
-            break;
-          case 'q':
-            sendCommand = "/move down";
-            break;
-          case 'e':
-            sendCommand = "/move up";
-            break;
-          case 'escape':
-            this.$store.dispatch('setObjectTarget', '');
-            break;
-          case 'enter':
-            this.$store.dispatch('setForceInputFocus', { forced: true });
-            break;
-          case '/':
-            this.$store.dispatch('setForceInputFocus', { forced: true, text: '/' });
-            break;
-        }
-
-        if (sendCommand.length > 0) {
-          this.$store.dispatch('sendSlashCommand', {
-            command: sendCommand
-          });
-        }
-      }
     },
 
     mounted() {
-      this.onWindowResize();
+        this.onWindowResize();
 
-      window.addEventListener(
-        'resize',
-        this.onWindowResize
-      );
+        window.addEventListener(
+            'resize',
+            this.onWindowResize
+        );
 
-      window.addEventListener(
-        'keyup',
-        this.onKeyUp
-      );
+        window.addEventListener(
+            'keyup',
+            this.onKeyUp
+        );
     },
 
     destroyed() {
-      window.removeEventListener(
-        'resize',
-        this.onWindowResize
-      );
+        window.removeEventListener(
+            'resize',
+            this.onWindowResize
+        );
     }
-  }
+}
 </script>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i');
-  @import url('https://fonts.googleapis.com/css?family=Inconsolata:400,700&display=swap');
-  $backgroundNormal: #111;
-  $backgroundLight: #1b1b1b;
-  $sidebarWidth: 250px;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i');
+@import url('https://fonts.googleapis.com/css?family=Inconsolata:400,700&display=swap');
+$backgroundNormal: #111;
+$backgroundLight: #1b1b1b;
+$sidebarWidth: 250px;
 
-  html, body {
+html, body {
     padding: 0;
     margin: 0;
     height: 100%;
     background-color: $backgroundNormal;
     user-select: none;
-  }
+}
 
-  ::-webkit-scrollbar { width: 3px; height: 3px; }
-  ::-webkit-scrollbar-button {  background-color: #666; }
-  ::-webkit-scrollbar-track {  background-color: #646464; }
-  ::-webkit-scrollbar-track-piece { background-color: #111; }
-  ::-webkit-scrollbar-thumb { height: 50px; background-color: #333; border-radius: 0px; }
-  ::-webkit-scrollbar-corner { background-color: #646464; }
-  ::-webkit-resizer { background-color: #666; }
+::-webkit-scrollbar { width: 3px; height: 3px; }
+::-webkit-scrollbar-button {  background-color: #666; }
+::-webkit-scrollbar-track {  background-color: #646464; }
+::-webkit-scrollbar-track-piece { background-color: #111; }
+::-webkit-scrollbar-thumb { height: 50px; background-color: #333; border-radius: 0px; }
+::-webkit-scrollbar-corner { background-color: #646464; }
+::-webkit-resizer { background-color: #666; }
 
-  #app {
+#app {
     font-family: 'Montserrat', sans-serif;
     /* -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale; */
@@ -203,69 +207,69 @@
     flex-direction: column;
     height: 100%;
 
-    .container-wrapper {
-      flex-grow: 1;
-      display: flex;
+.container-wrapper {
+    flex-grow: 1;
+    display: flex;
 
-      .container-left {
-        flex-basis: $sidebarWidth;
-        min-width: $sidebarWidth;
-        background-color: $backgroundLight;
-        display: flex;
-        flex-direction: column;
+.container-left {
+    flex-basis: $sidebarWidth;
+    min-width: $sidebarWidth;
+    background-color: $backgroundLight;
+    display: flex;
+    flex-direction: column;
 
-        .container-minimap {
-          flex-basis: 250px;
-        }
+.container-minimap {
+    flex-basis: 250px;
+}
 
-        .container-targets {
-          flex-grow: 1;
-          min-height: 100px;
-        }
-      }
+.container-targets {
+    flex-grow: 1;
+    min-height: 100px;
+}
+}
 
-      .container-center {
-        flex-grow: 1;
-        display: flex;
-        flex-direction: column;
-        box-shadow: 0px 0px 12px 1px #080808;
-        position: relative;
+.container-center {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0px 0px 12px 1px #080808;
+    position: relative;
 
-        .container-maintext {
-          flex-grow: 1;
-        }
+.container-maintext {
+    flex-grow: 1;
+}
 
-        .container-input {
-          flex-shrink: 1;
-        }
+.container-input {
+    flex-shrink: 1;
+}
 
-        .container-bars {
-          flex-basis: 30px;
-        }
-      }
+.container-bars {
+    flex-basis: 30px;
+}
+}
 
-      .container-right {
-        flex-basis: $sidebarWidth;
-        min-width: $sidebarWidth;
-        background-color: $backgroundLight;
-        display: flex;
-        flex-direction: column;
+.container-right {
+    flex-basis: $sidebarWidth;
+    min-width: $sidebarWidth;
+    background-color: $backgroundLight;
+    display: flex;
+    flex-direction: column;
 
-        .container-skills {
-          flex-grow: 1;
-        }
+.container-skills {
+    flex-grow: 1;
+}
 
-        .container-inventory {
-          flex-basis: 365px;
-        }
-      }
-    }
+.container-inventory {
+    flex-basis: 365px;
+}
+}
+}
 
-    .status-bar-container {
-      flex-basis: 30px;
-      border-top: 1px solid #333;
-      position: relative;
-      background-color: #0e0e0e;
-    }
-  }
+.status-bar-container {
+    flex-basis: 30px;
+    border-top: 1px solid #333;
+    position: relative;
+    background-color: #0e0e0e;
+}
+}
 </style>
