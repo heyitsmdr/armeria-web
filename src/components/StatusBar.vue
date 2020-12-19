1a487fe9fb29c41c33b7ac726012cbb986159fc9
Mike DuRussel <mdurussel@squarespace.com>
2020-12-18 20:23:04 -0500

    Show build/deploy info in StatusBar

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="root">
-        <div class="inner-container">Ping: {{pingTime}}ms</div>
+        <div class="inner-container">
+            <div class="ping">Ping: {{ pingTime }}ms</div>
+            <div class="version">
+                {{ deployAppName }}
+                <span v-if="deployVersion.length > 0">({{ deployVersion }})</span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,12 +15,12 @@
 
     export default {
         name: 'StatusBar',
-        computed: mapState(['pingTime']),
+        computed: mapState(['pingTime', 'deployAppName', 'deployVersion']),
     }
 </script>
 
 <style scoped lang="scss">
-    @import "@/styles/common";
+    @import "~@/styles/common";
 
     .root {
         display: flex;
@@ -22,6 +28,21 @@
         align-items: center;
         font-size: 12px;
         box-sizing: border-box;
+
+        .inner-container {
+            display: flex;
+            width: 100%;
+            padding: 0 5px;
+
+            .ping {
+                flex-shrink: 1;
+            }
+
+            .version {
+                flex-grow: 1;
+                text-align: right;
+            }
+        }
     }
 
     .ping {
