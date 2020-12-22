bc4594918969437e597bd5145470367685242a97
Mike DuRussel <mdurussel@squarespace.com>
2020-12-22 01:01:45 -0500

    Update data

--- data/scripts/mob-brenda.lua
+++ data/scripts/mob-brenda.lua
@@ -1,5 +1,17 @@
 function character_said(text)
   if text == "shop" then
     shop("WOBGI_TAVERN")
+  elseif text == "convo" then
+    say("What is it you'd like to learn more about?")
+    convo_select("weather", "Can you tell me about the weather here?")
+    convo_select("whereami", "What is this place?")
+  end
+end
+
+function conversation_select(option_id)
+  if option_id == "weather" then
+    say("The weather is very odd.. to say the least.")
+  elseif option_id == "whereami" then
+    say("I wish I knew.")
   end
 end
